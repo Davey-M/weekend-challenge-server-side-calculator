5419ab2409ed274c24691c46c934e6dbc28eb154
Davey-M <davey.meuer@gmail.com>
2022-02-04 16:06:06 -0600

    Added helper functions

--- server/server.js
+++ server/server.js
@@ -3,6 +3,25 @@ const app = express();
 
 const port = 5000;
 
+
+// HELPER FUNCTIONS -------------------------------------------
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+    return numOne / numTwo;
+}
+// HELPER FUNCTIONS -------------------------------------------
+
 app.use(express.static(__dirname + "/public"));
 
 app.listen(port, (err) => {
