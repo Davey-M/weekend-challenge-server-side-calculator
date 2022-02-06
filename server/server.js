a4c93b7246312efb0e89cf65f4cf8c0f3756c76a
Davey-M <davey.meuer@gmail.com>
2022-02-06 15:48:27 -0600

    Changes port env

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.port || 5000;
+const port = process.env.PORT ?? 5000;
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
